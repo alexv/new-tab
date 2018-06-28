4d04467b7eee4d221ab81dd0d67ccd8b85d8e74d
Alex Villarreal <avillarreal01@gmail.com>
2018-06-28 15:56:59 -0400

    inital dark mode

--- js/index.js
+++ js/index.js
@@ -3,3 +3,29 @@ import startProgress from './progress.js'
 
 startClock()
 startProgress()
+
+let darkMode = false;
+let darkStorage = localStorage.getItem('darkMode')
+if(darkStorage === 'true'){
+  // dark mode is on
+  console.log('Dark mode cookie found', darkStorage)
+  darkMode = true
+  document.getElementById('checkbox').removeAttribute('checked')
+  const elements = document.getElementsByTagName('*')
+  for (let i=0; i < elements.length; i++){
+     elements[i].classList.add('dark')
+  }
+}
+function toggleDarkMode (){
+  console.log('Dark Mode Toggled')
+  darkMode = !darkMode
+  const elements = document.getElementsByTagName('*')
+  for (let i=0; i < elements.length; i++){
+    elements[i].classList.toggle('dark')
+  }
+  localStorage.setItem('darkMode', darkMode)
+}
+const darkBtn = document.getElementById('checkbox')
+darkBtn.onclick = () => toggleDarkMode()
+
+
