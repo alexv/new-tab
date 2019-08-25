d951c4520485ae2a56aa32ed6257d9a63781234e
Alex Villarreal <avillarreal01@gmail.com>
2019-08-25 18:05:49 -0400

    prevent flicker

--- js/index.js
+++ js/index.js
@@ -1,31 +1,32 @@
-import startClock from './clock.js'
-import startProgress from './progress.js'
+import startClock from './clock.js';
+import startProgress from './progress.js';
 
-startClock()
-startProgress()
+startClock();
+startProgress();
 
 let darkMode = false;
-let darkStorage = localStorage.getItem('darkMode')
-if(darkStorage === 'true'){
+let darkStorage = localStorage.getItem('darkMode');
+if (darkStorage === 'true') {
   // dark mode is on
-  console.log('Dark mode cookie found', darkStorage)
-  darkMode = true
-  document.getElementById('checkbox').removeAttribute('checked')
-  const elements = document.getElementsByTagName('*')
-  for (let i=0; i < elements.length; i++){
-     elements[i].classList.add('dark')
+  console.log('Dark mode cookie found', darkStorage);
+  darkMode = true;
+  document.getElementById('checkbox').removeAttribute('checked');
+  const elements = document.getElementsByTagName('*');
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add('dark');
   }
 }
-function toggleDarkMode (){
-  console.log('Dark Mode Toggled')
-  darkMode = !darkMode
-  const elements = document.getElementsByTagName('*')
-  for (let i=0; i < elements.length; i++){
-    elements[i].classList.toggle('dark')
+function toggleDarkMode() {
+  console.log('Dark Mode Toggled');
+  darkMode = !darkMode;
+  const elements = document.getElementsByTagName('*');
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle('dark');
   }
-  localStorage.setItem('darkMode', darkMode)
+  localStorage.setItem('darkMode', darkMode);
 }
-const darkBtn = document.getElementById('checkbox')
-darkBtn.onclick = () => toggleDarkMode()
+const darkBtn = document.getElementById('checkbox');
+darkBtn.onclick = () => toggleDarkMode();
 
-
+// Display body after light/dark mode has been calculated to prevent screen flicker.
+document.getElementById('main').style.display = 'flex';
